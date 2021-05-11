61468c90f7c7a717c0341172e74629093e2e2c3a
deekshavempati <deekshaapple6@gmail.com>
2021-05-11 23:51:39 +0530

    Variables
    Signed-off-by: deekshavempati <deekshaapple6@gmail.com>

--- app/src/main/java/com/example/kotlin/Main.kt
+++ app/src/main/java/com/example/kotlin/Main.kt
@@ -1,5 +1,10 @@
 package com.example.kotlin
 
 fun main(){
-    print("Hello World")
+    var myString:String=("Android Studio")
+    var myFloat:Float=13.7f
+    val myLong:Long=1300000L
+    print("\n $myString")
+    print("\n $myFloat")
+    print("\n $myLong")
 }
