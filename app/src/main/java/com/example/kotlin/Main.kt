3c7072115869e57c6fa3aa67888e07948014219a
deekshavempati <deekshaapple6@gmail.com>
2021-05-13 21:11:40 +0530

    colors array
    Signed-off-by: deekshavempati <deekshaapple6@gmail.com>

--- app/src/main/java/com/example/kotlin/Main.kt
+++ app/src/main/java/com/example/kotlin/Main.kt
@@ -1,10 +1,14 @@
 package com.example.kotlin
 
 fun main(){
-    var myString:String=("Android Studio")
-    var myFloat:Float=13.7f
-    val myLong:Long=1300000L
-    print("\n $myString")
-    print("\n $myFloat")
-    print("\n $myLong")
+    var n = 11
+    var res:String =if(n>9) "hello " else "world"
+
+    res=when (n){
+        1 ->("hello")
+        in 2..10 ->("world")
+        11,12,13 -> ("test")
+        else -> (" no")
+    }
+    print(res)
 }
